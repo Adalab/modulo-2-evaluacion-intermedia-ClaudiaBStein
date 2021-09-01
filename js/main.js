e61a903578bcc98e07491ed517b2fc9971a76503
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-01 10:37:31 +0200

    commentary written

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+//variables
 const inputNumber = document.querySelector('.js-number');
 const tryButton = document.querySelector('.js-button');
 const clueElement = document.querySelector('.js-clue');
@@ -7,10 +9,14 @@ const countElement = document.querySelector('.js-tries');
 const randomNumber = getRandomNumber (100);
 let tryNumber = 0;
 
+//función para generar el número
+
 function getRandomNumber(max){
     return randomNumber = Math.ceil(Math.random()*max);
 }
 console.log('El número aleatorio es ${randomNumber}');
+
+//función del mecanismo
 
 function guessRandomNumber(){
 const inputNumberValue = inputNumber.value;
@@ -27,3 +33,15 @@ else if (guessRandomNumber<1 || guessRandomNumber>100){
     clueElement.innerHTML = 'El número debe estar entre 1 y 100.';
 }
 }
+
+//función de intentos
+
+function countTries() {
+  tryNumber = numberTries + 1;
+  inputNumber.value = 'Número de intentos: ' + inputNumber;
+}
+
+//evento del botón
+
+tryButton.addEventListener('click', guessRandomNumber);
+tryButton.addEventListener = ('click', countTries);
