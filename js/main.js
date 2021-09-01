0ee40ef58ff1f82d5255f2991c142daf6b9aefe0
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-01 10:27:53 +0200

    if and else if added

--- js/main.js
+++ js/main.js
@@ -4,14 +4,26 @@ const inputNumber = document.querySelector('.js-number');
 const tryButton = document.querySelector('.js-button');
 const clueElement = document.querySelector('.js-clue');
 const countElement = document.querySelector('.js-tries');
+const randomNumber = getRandomNumber (100);
 let tryNumber = 0;
 
 function getRandomNumber(max){
     return randomNumber = Math.ceil(Math.random()*max);
-
 }
+console.log('El número aleatorio es ${randomNumber}');
 
 function guessRandomNumber(){
-const randomNumber = getRandomNumber();
-if (getRandomNumber===)
+const inputNumberValue = inputNumber.value;
+if (guessRandomNumber===getRandomNumber){
+    clueElement.classList.add ('¡Has ganado, campeona!');
+}
+else if (guessRandomNumber>=getRandomNumber){
+    clueElement.innerHTML = 'Demasiado alto.';
+}
+else if (guessRandomNumber<=getRandomNumber){
+    clueElement.innerHTML = 'Demasiado bajo.';
+}
+else if (guessRandomNumber<1 || guessRandomNumber>100){
+    clueElement.innerHTML = 'El número debe estar entre 1 y 100.';
+}
 }
