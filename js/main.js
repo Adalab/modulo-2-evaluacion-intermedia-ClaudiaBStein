bfca4461fdf0af0751acac0a563e78fa7e840a0e
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-01 10:11:54 +0200

    js const added

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const inputNumber = document.querySelector('.js-number');
+const tryButton = document.querySelector('.js-button');
+const clueElement = document.querySelector('.js-clue');
+const countElement = document.querySelector('.js-tries');
+let tryNumber = 0;
+
+function getRandomNumber(max){
+    return randomNumber = Math.ceil(Math.random()*max);
+
+}
+
+function guessRandomNumber(){
+const randomNumber = getRandomNumber();
+if (getRandomNumber===)
+}
